1858edc062b642d7afc06137de1682df3e8d345d
Aadil Matloob <8232aadil@gmail.com>
2023-04-07 23:59:53 +0530

    initial

---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,25 @@
+import Container from "../Container";
+function Navbar() {
+  return (
+    <div className=" fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="
+            py-4
+            border-b-[1px]"
+      >
+        <Container>
+            <div className="
+            flex
+            flex-row
+            items-center
+            justify-between
+            ">
+
+             </div>
+        </Container>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
